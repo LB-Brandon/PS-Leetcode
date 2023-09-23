7ba057860ca7b5460ba2e43261b521a5fc7ac2bc
Keun Young Song <rmsdud623@gmail.com>
2023-09-23 11:43:55 +0900

    Time: 2506 ms (23.97%), Space: 17.2 MB (58.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # brute-force
-        # for i in range(len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return [i, j]
-        # return []
+        def bf():
+            for i in range(len(nums)):
+                for j in range(i+1, len(nums)):
+                    if nums[i] + nums[j] == target:
+                        return [i, j]
+            return []
 
-        # using in and slicing
-        for i, n in enumerate(nums):
-            complement = target - n
-            if complement in nums[i+1:]:
-                return [i, nums[i+1:].index(complement)+i+1]
+        def in_slicing():
+            for i, n in enumerate(nums):
+                complement = target - n
+                if complement in nums[i+1:]:
+                    return [i, nums[i+1:].index(complement)+i+1]
+
+        # excute
+        return bf()
                 
