8951997c3265fdafc0232f4417603f5e14f6ba22
Keun Young Song <rmsdud623@gmail.com>
2023-09-23 11:51:16 +0900

    Time: 62 ms (77.68%), Space: 17.6 MB (30.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -8,11 +8,20 @@ class Solution:
             return []
 
         def in_slicing():
-            for i, n in enumerate(nums):
-                complement = target - n
+            for i, num in enumerate(nums):
+                complement = target - num
                 if complement in nums[i+1:]:
                     return [i, nums[i+1:].index(complement)+i+1]
 
+        def map():
+            nums_map = {}
+            for i, num in enumerate(nums):
+                nums_map[num] = i
+            for i, num in enumerate(nums):
+                if target - num in nums_map and i != nums_map[target-num]:
+                    return [i, nums_map[target-num]]
+            return []
+
         # excute
-        return bf()
+        return map()
                 
