f99603162a59134e0f456dc0a731f63247cd1230
Keun Young Song <rmsdud623@gmail.com>
2023-09-25 14:40:44 +0900

    Time: 9200 ms (5%), Space: 18.9 MB (8.37%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,18 +1,36 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left, right = 0, len(height)-1
-        left_max, right_max = height[left], height[right]
-        output = 0
-        while left < right:
-            left_max = max(left_max, height[left])
-            right_max = max(right_max, height[right])
-            if left_max <= right_max:
-                output += left_max - height[left]
-                left += 1
-            else:
-                output += right_max - height[right]
-                right -= 1
-        return output
+
+        def BF():
+            output = 0
+            # can't use max() for empty list
+            for i in range(1, len(height)-1):
+                left_max = max(height[:i])
+                right_max = max(height[i+1:])
+
+                min_height = min(left_max, right_max)
+                if min_height > height[i]:
+                    output += min_height - height[i]
+            return output
+                
+                
+        # two pointer
+        def two_wall():
+            left, right = 0, len(height)-1
+            left_max, right_max = height[left], height[right]
+            output = 0
+            while left < right:
+                left_max = max(left_max, height[left])
+                right_max = max(right_max, height[right])
+                if left_max <= right_max:
+                    output += left_max - height[left]
+                    left += 1
+                else:
+                    output += right_max - height[right]
+                    right -= 1
+            return output
+        
+        return BF()
 
                 
             
