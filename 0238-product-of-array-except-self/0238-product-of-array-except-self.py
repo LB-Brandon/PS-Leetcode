3d7dd30d5ac934095d6cdd5732e5d68a849d9fe7
Keun Young Song <rmsdud623@gmail.com>
2023-09-27 15:00:23 +0900

    Time: 208 ms (18.48%), Space: 24.1 MB (46.93%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -72,4 +72,21 @@ class Solution:
                 output.append(prefix[i] * postfix[i])
             return output
         
-        return sol4()
+        def sol5():
+            n = len(nums)
+            output = [1] * n
+            left_product = 1
+            right_product = 1
+
+            for i in range(n):
+                output[i] = left_product
+                left_product *= nums[i]
+            print(output)
+            for i in range(n-1, -1, -1):
+                output[i] = right_product * output[i]
+                right_product *= nums[i]
+            print(output)
+            return output
+
+        
+        return sol5()
