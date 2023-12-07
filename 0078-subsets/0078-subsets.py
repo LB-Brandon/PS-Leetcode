25da0e511b2850f1eb892588eb6ab5cf93db9c63
Keunyoung Song <rmsdud623@gmail.com>
2023-12-07 09:14:12 +0900

    Time: 47 ms (17.03%), Space: 16.7 MB (24.14%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,31 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         subset = []
-        def dfs(i):
-            if i >= len(nums):
+        
+        def dfs(depth):
+            if depth == len(nums):
                 result.append(subset.copy())
                 return
             
-            # decision to include nums[i]
-            subset.append(nums[i])
-            dfs(i + 1)
+            subset.append(nums[depth])
+            dfs(depth+1)
             
-            #decision Not to include nums[i]
             subset.pop()
-            dfs(i+1)
+            dfs(depth+1)
+            
         
         result = []
         dfs(0)
         return result
-    
-#     ----------------------------------------
-#     def dfs(start, path):
-#             result.append(path)
-#             # print(start, start*"  ", path)
-
-#             for i in range(start, len(nums)):
-#                 dfs(i+1, path+[nums[i]])
-
-#     result = []
-#     dfs(0, [])
-#     return result
+        
