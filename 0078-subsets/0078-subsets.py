a3f2163b1ffa74c35218894e4d996d5aea1c5f39
Keunyoung Song <rmsdud623@gmail.com>
2023-12-07 09:35:21 +0900

    Time: 41 ms (53.79%), Space: 16.5 MB (24.14%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,27 +1,27 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-#         subset = []
+        subset = []
         
-#         def dfs(depth):
-#             if depth == len(nums):
-#                 result.append(subset.copy())
-#                 return
+        def dfs(depth):
+            if depth == len(nums):
+                result.append(subset.copy())
+                return
             
-#             subset.append(nums[depth])
-#             dfs(depth+1)
+            subset.append(nums[depth])
+            dfs(depth+1)
             
-#             subset.pop()
-#             dfs(depth+1)
+            subset.pop()
+            dfs(depth+1)
 
-        def dfs(start, path):
-            if start > len(nums): return
+#         def dfs(start, path):
+#             if start > len(nums): return
             
-            result.append(path.copy())
+#             result.append(path.copy())
             
-            for i in range(start, len(nums)):
-                dfs(i+1, path+[nums[i]])
+#             for i in range(start, len(nums)):
+#                 dfs(i+1, path+[nums[i]])
                 
         result = []
-        dfs(0, [])
+        dfs(0)
         return result
         
