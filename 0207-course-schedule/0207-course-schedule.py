7101dbcb4a969c6a4e3a2390b7e084272688ccdd
Keunyoung Song <rmsdud623@gmail.com>
2023-12-12 20:23:55 +0900

    Time: 102 ms (39.96%), Space: 19.5 MB (9.02%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,33 +1,29 @@
 class Solution:
     def canFinish(self, numCourses, prerequisites):
-        if not prerequisites: return True
+
         graph = defaultdict(list)
 
         for f, t in prerequisites:
             graph[f].append(t)
 
-        traced = set()
         visited = set()
-        
+        path = set()
+
         def dfs(cur):
-            if cur in traced:
+            if cur in path:
                 return False
             if cur in visited:
                 return True
-            
-            traced.add(cur)
+
+            visited.add(cur)
             for nextNode in graph[cur]:
+                path.add(cur)
                 if not dfs(nextNode):
                     return False
-            
-            traced.remove(cur)
-            visited.add(cur)
-            
+                path.remove(cur)
             return True
-            
-        for node in list(graph):
-            if not dfs(node):
+
+        for i in range(numCourses):
+            if not dfs(i):
                 return False
-        
         return True
-                    
